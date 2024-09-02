e705039c6031deb6e91482cabd883515b725eef4
rahul-2271 <rahuld201976@gmail.com>
2024-09-02 22:02:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -88,8 +88,7 @@ app.post("/user", async (req, res) => {
 });
 
 app.post("/new", async (req, res) => {
-  //Hint: The RETURNING keyword can return the data that was inserted.
-  //https://www.postgresql.org/docs/current/dml-returning.html
+  
   const name = req.body.name;
   const color = req.body.color;
 
